30a3c236b47bde7f3cd43320e4ec4eeea000cd30
Akshay Kalra <akshaykalra10@gmail.com>
2014-10-12 18:40:30 +0530

    datbase

--- app/src/main/java/com/mobappclub/waves2014/ScheduleActivity.java
+++ app/src/main/java/com/mobappclub/waves2014/ScheduleActivity.java
@@ -1,98 +1,229 @@
 package com.mobappclub.waves2014;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
 import android.app.Activity;
+import android.app.ActionBar;
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Context;
+import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TabHost;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
 
-/**
- * Created by Akshay on 03-10-2014.
- */
-public class ScheduleActivity extends Activity
-{
-    List<ScheduleObject> list1,list2,list3,list4;
-    MyAdapter adapt1,adapt2,adapt3,adapt4;
-    protected DatabaseHandler db;
+public class ScheduleActivity extends Activity implements ActionBar.TabListener {
 
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v13.app.FragmentStatePagerAdapter}.
+     */
+    SectionsPagerAdapter mSectionsPagerAdapter;
 
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_schedule);
+        setContentView(R.layout.activity_schedule_activity2);
 
-        TabHost host = (TabHost) findViewById(R.id.tabhost);
-        host.setup();
+        // Set up the action bar.
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        TabHost.TabSpec spec = host.newTabSpec("Tab One");
-        spec.setContent(R.id.tab1);
-        spec.setIndicator("DAY 0");
-        host.addTab(spec);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
 
-        spec = host.newTabSpec("Tab Two");
-        spec.setContent(R.id.tab2);
-        spec.setIndicator("DAY 1");
-        host.addTab(spec);
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        spec = host.newTabSpec("Tab Two");
-        spec.setContent(R.id.tab3);
-        spec.setIndicator("DAY 2");
-        host.addTab(spec);
+        // When swiping between different sections, select the corresponding
+        // tab. We can also use ActionBar.Tab#select() to do this if we have
+        // a reference to the Tab.
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
 
+        ActionBar.Tab tab[] = new ActionBar.Tab[4];
+        // For each of the sections in the app, add a tab to the action bar.
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+            // Create a tab with text corresponding to the page title defined by
+            // the adapter. Also specify this Activity object, which implements
+            // the TabListener interface, as the callback (listener) for when
+            // this tab is selected.
 
-        spec = host.newTabSpec("Tab Two");
-        spec.setContent(R.id.tab4);
-        spec.setIndicator("DAY 3");
-        host.addTab(spec);
-        host.setCurrentTab(1);
+            tab[i] = actionBar.newTab()
+                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(this);
+            actionBar.addTab(tab[i]);
+        }
+        int j=0;
+        Calendar rightnow= Calendar.getInstance();
+        if(rightnow.get(Calendar.DAY_OF_MONTH)==8 &&rightnow.get(Calendar.MONTH)==9)
+        {
+           j=1;
+        }
+        else
+        {
+            j=2;
+        }
 
-        db = new DatabaseHandler(this);
-        list1 = db.getAllEvents(0);
-        adapt1 = new MyAdapter(this, R.layout.schedule_list_inner_view, list1);
-        ListView listTask1 = (ListView) findViewById(R.id.listView1);
-        listTask1.setAdapter(adapt1);
-
-        list2 = db.getAllEvents(1);
-        adapt2 = new MyAdapter(this, R.layout.schedule_list_inner_view, list2);
-        ListView listTask2 = (ListView) findViewById(R.id.listView2);
-        listTask2.setAdapter(adapt2);
-
-
-        list3 = db.getAllEvents(2);
-        adapt3 = new MyAdapter(this, R.layout.schedule_list_inner_view, list3);
-        ListView listTask3 = (ListView) findViewById(R.id.listView3);
-        listTask3.setAdapter(adapt3);
-
-
-        list4 = db.getAllEvents(3);
-        adapt4 = new MyAdapter(this, R.layout.schedule_list_inner_view, list4);
-        ListView listTask4 = (ListView) findViewById(R.id.listView4);
-        listTask4.setAdapter(adapt4);
-
-
-
-
+       tab[j].select();
     }
 
-    private class MyAdapter extends ArrayAdapter<ScheduleObject> {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.schedule_activity2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, switch to the corresponding page in
+        // the ViewPager.
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return new PlaceholderFragment().newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 4;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return "DAY 0";
+                case 1:
+                    return "DAY 1";
+                case 2:
+                    return "DAY 2";
+                case 3:
+                    return "DAY 3";
+            }
+            return null;
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String DAY_NUMBER = "day_number";
+        List<EventsObject> list1;
+        MyAdapter adapt1;
+        protected DatabaseHandler db;
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int dayNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(DAY_NUMBER, dayNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_schedule_activity2, container, false);
+            Bundle args=getArguments();
+            int dayNumber=args.getInt(DAY_NUMBER);
+            db=new DatabaseHandler(getActivity()) ;
+            list1 = db.getDayEvents(dayNumber);
+            adapt1 = new MyAdapter(getActivity(), R.layout.schedule_list_inner_view, list1);
+            ListView listTask1 = (ListView) rootView.findViewById(R.id.listView1);
+            listTask1.setAdapter(adapt1);
+            return rootView;
+
+        }
+    }
+
+    public static class MyAdapter extends ArrayAdapter<EventsObject> {
         Context context;
-        List<ScheduleObject> scheduleList = new ArrayList<ScheduleObject>();
+        List<EventsObject> scheduleList = new ArrayList<EventsObject>();
         int layoutResourceId;
 
         public MyAdapter(Context context, int layoutResourceId,
-                         List<ScheduleObject> objects) {
+                         List<EventsObject> objects) {
             super(context, layoutResourceId, objects);
             this.layoutResourceId = layoutResourceId;
             this.scheduleList = objects;
@@ -113,15 +244,13 @@ public class ScheduleActivity extends Activity
             } else {
                 chk = (TextView) convertView.getTag();
             }
-            ScheduleObject current=scheduleList.get(position);
-            chk.setText(current.getStart_time()+" "+current.getEnd_time()+" "+current.getEvent_name());
+            EventsObject current=scheduleList.get(position);
+            chk.setText(current.getStart_time()+" "+current.getEnd_time()+" "+current.getName());
             chk.setTag(current);
             return convertView;
 
         }
 
     }
+
 }
-
-
-
