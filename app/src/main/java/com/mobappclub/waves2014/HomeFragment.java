95d2816520047d7a5ea364dd39cf97675439f8d5
Akshay Kalra <akshaykalra10@gmail.com>
2014-10-12 18:47:23 +0530

    Missing Files

---
+++ app/src/main/java/com/mobappclub/waves2014/HomeFragment.java
@@ -0,0 +1,40 @@
+package com.mobappclub.waves2014;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Akshay on 10-10-2014.
+ */
+public class HomeFragment extends Fragment {
+    public HomeFragment() {
+        super();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        return rootView;
+    }
+}
