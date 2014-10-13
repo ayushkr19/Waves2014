e0a2531ac0ea2c77309d48506ff1e8c9b5d67665
Ayush Kumar <ayushkr19@gmail.com>
2014-10-13 16:03:34 +0530

    Design

--- app/src/main/java/com/mobappclub/waves2014/NavigationDrawerFragment.java
+++ app/src/main/java/com/mobappclub/waves2014/NavigationDrawerFragment.java
@@ -4,6 +4,8 @@ package com.mobappclub.waves2014;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
+import android.os.Build;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,16 +13,21 @@ import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 import java.util.ArrayList;
 
