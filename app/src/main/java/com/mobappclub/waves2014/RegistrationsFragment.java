6046b83f10d408652966b808359ee65e583d59ba
Akshay Kalra <akshaykalra10@gmail.com>
2014-10-13 21:29:22 +0530

    Sponsor Database

---
+++ app/src/main/java/com/mobappclub/waves2014/RegistrationsFragment.java
@@ -0,0 +1,41 @@
+package com.mobappclub.waves2014;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Akshay on 12-10-2014.
+ */
+public class RegistrationsFragment extends Fragment {
+
+    public  RegistrationsFragment() {
+        super();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_registrations, container, false);
+        return rootView;
+    }
+}
